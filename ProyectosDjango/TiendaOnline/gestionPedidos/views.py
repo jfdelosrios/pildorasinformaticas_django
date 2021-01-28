fa61274188532f828df84e35572e771098ead479
Jorge Fernando De Los Rios <jfdelosrios@hotmail.com>
2021-01-28 09:01:30 -0500

    Finalizado video 23

--- ProyectosDjango/TiendaOnline/gestionPedidos/views.py
+++ ProyectosDjango/TiendaOnline/gestionPedidos/views.py
@@ -17,11 +17,24 @@ def buscar(request):
 
         producto=request.GET['prd']
 
-        articulos=Articulos.objects.filter(nombre__icontains=producto)
+        if(len(producto)>20):
+            mensaje='Texto de busqueda demasiado largo'
+        else:
 
-        return render(request, 'resultados_busqueda.html', {'articulos': articulos, 'query': producto})
+            articulos=Articulos.objects.filter(nombre__icontains=producto)
+
+            return render(request, 'resultados_busqueda.html', {'articulos': articulos, 'query': producto})
     
     else:
         mensaje='No has introducido nada'
 
-        return HttpResponse(mensaje)
+    return HttpResponse(mensaje)
+
+
+def contacto(request):
+
+    if request.method=='POST':
+
+        return render(request, 'gracias.html')
+
+    return render(request, 'contacto.html')

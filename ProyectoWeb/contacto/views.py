016eb3f32deba6a59b5150ee766913361a78fe36
Jorge Fernando De Los Rios <jfdelosrios@hotmail.com>
2021-02-13 21:39:37 -0500

    * Solucionado problema de carga del formulario. * Solucionado problema con migrate.

--- ProyectoWeb/contacto/views.py
+++ ProyectoWeb/contacto/views.py
@@ -4,8 +4,7 @@ from .forms import FormularioContacto
 
 # Create your views here.
 
-
-
 def contacto(request):
+
     formulario_contacto=FormularioContacto()
     return render(request,'contacto/contacto.html',{'miFormulario': formulario_contacto})
